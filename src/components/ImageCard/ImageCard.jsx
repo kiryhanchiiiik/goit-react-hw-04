422c02b6c6a1d4be7ae00deac9ceb07194c81391
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2024-10-10 20:36:10 +0300

    111

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,11 +2,9 @@ import css from "./ImageCard.module.css";
 
 const ImageCard = ({ photo, onImageClick }) => {
   return (
-    <div
-      className={css.container}
-      onClick={() => onImageClick(photo.urls.regular, photo.alt_description)}
-    >
+    <div className={css.container}>
       <img
+        onClick={() => onImageClick(photo.urls.regular, photo.alt_description)}
         className={css.photo}
         src={photo.urls.small}
         alt={photo.alt_description}
